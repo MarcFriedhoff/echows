1849636c37bec70274f2007d98dbde8b2f6a5ffd
MarcFriedhoff <marc.friedhoff@ibm.com>
2024-09-19 19:06:29 +0200

    New

--- code/source/echows/admin.java
+++ code/source/echows/admin.java
@@ -7,6 +7,9 @@ import com.wm.util.Values;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 // --- <<IS-END-IMPORTS>> ---
 
 public final class admin
@@ -34,7 +37,22 @@ public final class admin
 		StringBuffer buffer = new StringBuffer();
 		
 		buffer.append("current dir: " + System.getProperty("user.dir"));
+		buffer.append(System.lineSeparator());
 		
+		String filePath = "/opt/softwareag/IntegrationServer/config/aclmap_sm.conf";
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+		    String line;
+		    while ((line = reader.readLine()) != null) {
+		        buffer.append(line);
+		        buffer.append(System.lineSeparator());
+		    }
+		} catch (IOException e) {
+		    throw new ServiceException(e);
+		}
+		
+		// Print the content of the file
+		System.out.println(buffer.toString());
 		
 		// pipeline
 		IDataCursor pipelineCursor = pipeline.getCursor();
